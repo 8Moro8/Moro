7b3d6efe57ab52e89b925b7881fd1843c79f6cda
8Moro8 <169149172+8Moro8@users.noreply.github.com>
2024-05-07 23:03:22 +0500

    Update Untitled7.py

--- Untitled7.py
+++ Untitled7.py
@@ -1,22 +1,16 @@
+import streamlit as st
 import pandas as pd
 import requests
-from io import BytesIO
-import streamlit as st
 
 def load_and_process_data(country, file_path_prefix):
-
-    base_url = 'https://github.com/8Moro8/Moro/blob/main/'
-
     F_ad_Prob_Mod_Sev_values = []
 
     for year in range(2014, 2018):
         file_name = f"{file_path_prefix}_{year}.xlsx"
-        file_url = base_url + file_name
-        response = requests.get(file_url)
+        file_url = f"https://github.com/8Moro8/Moro/blob/main/{file_name}"
         
-        if response.status_code == 200:
-            excel_data = response.content
-            pandas_df = pd.read_excel(BytesIO(excel_data))
+        try:
+            pandas_df = pd.read_excel(file_url)
             F_ad_Prob_Mod_Sev = (pandas_df['Prob_Mod_Sev'] * pandas_df['wt']).sum() / pandas_df['wt'].sum()
             F_ad_Prob_Mod_Sev_values.append(float(F_ad_Prob_Mod_Sev))
 
