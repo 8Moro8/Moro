b2f992ad3980d861eadf163ce532f3ed899d3ae8
8Moro8 <169149172+8Moro8@users.noreply.github.com>
2024-05-07 23:52:38 +0500

    Update Untitled7.py

--- Untitled7.py
+++ Untitled7.py
@@ -2,22 +2,22 @@ import streamlit as st
 import pandas as pd
 
 excel_urls = {
-    'kaz_2014': 'https://github.com/8Moro8/Moro/blob/main/kaz_2014.xlsx',
-    'kaz_2015': 'https://github.com/8Moro8/Moro/blob/main/kaz_2015.xlsx',
-    'kaz_2016': 'https://github.com/8Moro8/Moro/blob/main/kaz_2016.xlsx',
-    'kaz_2017': 'https://github.com/8Moro8/Moro/blob/main/kaz_2017.xlsx',
-    'kgz_2014': 'https://github.com/8Moro8/Moro/blob/main/kgz_2014.xlsx',
-    'kgz_2015': 'https://github.com/8Moro8/Moro/blob/main/kgz_2015.xlsx',
-    'kgz_2016': 'https://github.com/8Moro8/Moro/blob/main/kgz_2016.xlsx',
-    'kgz_2017': 'https://github.com/8Moro8/Moro/blob/main/kgz_2017.xlsx',
-    'tjk_2014': 'https://github.com/8Moro8/Moro/blob/main/tjk_2014.xlsx',
-    'tjk_2015': 'https://github.com/8Moro8/Moro/blob/main/tjk_2015.xlsx',
-    'tjk_2016': 'https://github.com/8Moro8/Moro/blob/main/tjk_2016.xlsx',
-    'tjk_2017': 'https://github.com/8Moro8/Moro/blob/main/tjk_2017.xlsx',
-    'uzb_2014': 'https://github.com/8Moro8/Moro/blob/main/uzb_2014.xlsx',
-    'uzb_2015': 'https://github.com/8Moro8/Moro/blob/main/uzb_2015.xlsx',
-    'uzb_2016': 'https://github.com/8Moro8/Moro/blob/main/uzb_2016.xlsx',
-    'uzb_2017': 'https://github.com/8Moro8/Moro/blob/main/uzb_2017.xlsx'
+    'kaz_2014': 'https://github.com/8Moro8/Moro/raw/main/kaz_2014.xlsx',
+    'kaz_2015': 'https://github.com/8Moro8/Moro/raw/main/kaz_2015.xlsx',
+    'kaz_2016': 'https://github.com/8Moro8/Moro/raw/main/kaz_2016.xlsx',
+    'kaz_2017': 'https://github.com/8Moro8/Moro/raw/main/kaz_2017.xlsx',
+    'kgz_2014': 'https://github.com/8Moro8/Moro/raw/main/kgz_2014.xlsx',
+    'kgz_2015': 'https://github.com/8Moro8/Moro/raw/main/kgz_2015.xlsx',
+    'kgz_2016': 'https://github.com/8Moro8/Moro/raw/main/kgz_2016.xlsx',
+    'kgz_2017': 'https://github.com/8Moro8/Moro/raw/main/kgz_2017.xlsx',
+    'tjk_2014': 'https://github.com/8Moro8/Moro/raw/main/tjk_2014.xlsx',
+    'tjk_2015': 'https://github.com/8Moro8/Moro/raw/main/tjk_2015.xlsx',
+    'tjk_2016': 'https://github.com/8Moro8/Moro/raw/main/tjk_2016.xlsx',
+    'tjk_2017': 'https://github.com/8Moro8/Moro/raw/main/tjk_2017.xlsx',
+    'uzb_2014': 'https://github.com/8Moro8/Moro/raw/main/uzb_2014.xlsx',
+    'uzb_2015': 'https://github.com/8Moro8/Moro/raw/main/uzb_2015.xlsx',
+    'uzb_2016': 'https://github.com/8Moro8/Moro/raw/main/uzb_2016.xlsx',
+    'uzb_2017': 'https://github.com/8Moro8/Moro/raw/main/uzb_2017.xlsx'
 }
 
 # Выбор файла
