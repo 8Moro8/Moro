ccc82336184fc1619f979795455ac1f33b2b1540
8Moro8 <169149172+8Moro8@users.noreply.github.com>
2024-05-07 23:33:26 +0500

    Update Untitled7.py

--- Untitled7.py
+++ Untitled7.py
@@ -1,21 +1,25 @@
 import streamlit as st
 import pandas as pd
+import plotly as pt
 
-def load_and_process_data(country, file_path_prefix):
-    F_ad_Prob_Mod_Sev_values = []
-
-    for year in range(2014, 2018):
-        file_name = f"{file_path_prefix}_{year}.xlsx"
-        file_url = f"https://github.com/8Moro8/Moro/blob/main/{file_name}"
-        
-        try:
-            pandas_df = pd.read_excel(file_url, engine='openpyxl')  # Указываем движок явно
-            F_ad_Prob_Mod_Sev = (pandas_df['Prob_Mod_Sev'] * pandas_df['wt']).sum() / pandas_df['wt'].sum()
-            F_ad_Prob_Mod_Sev_values.append(float(F_ad_Prob_Mod_Sev))
-        except Exception as e:
-            st.error(f"Ошибка при обработке файла {file_name}: {e}")
-
-    return F_ad_Prob_Mod_Sev_values
+excel_urls = {
+    'kaz_2014': 'https://github.com/8Moro8/Moro/blob/main/kaz_2014.xlsx'
+    'kaz_2015': 'https://github.com/8Moro8/Moro/blob/main/kaz_2015.xlsx'
+    'kaz_2016': 'https://github.com/8Moro8/Moro/blob/main/kaz_2016.xlsx'
+    'kaz_2017': 'https://github.com/8Moro8/Moro/blob/main/kaz_2017.xlsx'
+    'kgz_2014': 'https://github.com/8Moro8/Moro/blob/main/kgz_2014.xlsx'
+    'kgz_2015': 'https://github.com/8Moro8/Moro/blob/main/kgz_2015.xlsx'
+    'kgz_2016': 'https://github.com/8Moro8/Moro/blob/main/kgz_2016.xlsx'
+    'kgz_2017': 'https://github.com/8Moro8/Moro/blob/main/kgz_2017.xlsx'
+    'tjk_2014': 'https://github.com/8Moro8/Moro/blob/main/tjk_2014.xlsx'
+    'tjk_2015': 'https://github.com/8Moro8/Moro/blob/main/tjk_2015.xlsx'
+    'tjk_2016': 'https://github.com/8Moro8/Moro/blob/main/tjk_2016.xlsx'
+    'tjk_2017': 'https://github.com/8Moro8/Moro/blob/main/tjk_2017.xlsx'
+    'uzb_2014': 'https://github.com/8Moro8/Moro/blob/main/uzb_2014.xlsx'
+    'uzb_2015': 'https://github.com/8Moro8/Moro/blob/main/uzb_2015.xlsx'
+    'uzb_2016': 'https://github.com/8Moro8/Moro/blob/main/uzb_2016.xlsx'
+    'uzb_2017': 'https://github.com/8Moro8/Moro/blob/main/uzb_2017.xlsx'
+}
 
 # Загрузка данных
 countries = ['Казахстан', 'Кыргызстан', 'Таджикистан', 'Узбекистан']
