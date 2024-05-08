82ead0bd3e03d5fa03f336bc6a43cc7fb6c11438
8Moro8 <169149172+8Moro8@users.noreply.github.com>
2024-05-08 19:52:42 +0500

    Update Untitled7.py

--- Untitled7.py
+++ Untitled7.py
@@ -3,14 +3,24 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Значения для графиков
+st.markdown(
+    """
+    <style>
+    body {
+        background-image: url('https://github.com/8Moro8/Moro/raw/main/1489.jpg');
+        background-size: cover;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 F_ad_Prob_Mod_Sev_kaz_values = [0.0737473506983265, 0.044529239425859325, 0.07208697980276833, 0.09025550050680399]
 F_ad_Prob_Mod_Sev_uzb_values = [0.09872602667454446, 0.12482079148104783, 0.1033934827101725, 0.16342414956949367]
 F_ad_Prob_Mod_Sev_tjk_values = [0.13234311608076732, 0.11086727498562164, 0.19598527440470287, 0.23921461895440915]
 F_ad_Prob_Mod_Sev_kgz_values = [0.1875365079274166, 0.21059007745097164, 0.19581301002148638, 0.19449654750600165]
 years = [2014, 2015, 2016, 2017]
 
-# Вывод графиков по отдельности
 def plot_country_graph(country, values, years):
     fig, ax = plt.subplots(figsize=(10, 6))
     ax.plot(years, values, marker='o', linestyle='-')
@@ -20,7 +30,6 @@ def plot_country_graph(country, values, years):
     ax.grid(True)
     st.pyplot(fig)
 
-# Выбор графика с помощью кнопки
 button = st.selectbox('Выберите график', ['Казахстан', 'Узбекистан', 'Таджикистан', 'Кыргызстан', 'Центральная Азия'])
 
 if button == 'Казахстан':
@@ -43,16 +52,3 @@ else:
     ax.legend()
     ax.grid(True)
     st.pyplot(fig)
-
-# Установка фона страницы
-st.markdown(
-    """
-    <style>
-    body {
-        background-image: url('https://github.com/8Moro8/Moro/raw/main/1489.jpg');
-        background-size: cover;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
