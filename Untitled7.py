fc074c94030132eb7d7010ee0a13e9d5f5540c8f
8Moro8 <169149172+8Moro8@users.noreply.github.com>
2024-05-08 22:41:18 +0500

    Update Untitled7.py

--- Untitled7.py
+++ Untitled7.py
@@ -35,11 +35,13 @@ def plot_country_graph(country, values, years):
     ax.grid(True)
     st.pyplot(fig)
 
-if st.button('Казахстан'):
-    plot_country_graph('Казахстан', F_ad_Prob_Mod_Sev_kaz_values, years)
-elif st.button('Узбекистан'):
-    plot_country_graph('Узбекистан', F_ad_Prob_Mod_Sev_uzb_values, years)
-elif st.button('Таджикистан'):
-    plot_country_graph('Таджикистан', F_ad_Prob_Mod_Sev_tjk_values, years)
-elif st.button('Кыргызстан'):
-    plot_country_graph('Кыргызстан', F_ad_Prob_Mod_Sev_kgz_values, years)
+with st.sidebar:
+    st.header("Выберите страну")
+    if st.sidebar.button('Казахстан'):
+        plot_country_graph('Казахстан', F_ad_Prob_Mod_Sev_kaz_values, years)
+    elif st.sidebar.button('Узбекистан'):
+        plot_country_graph('Узбекистан', F_ad_Prob_Mod_Sev_uzb_values, years)
+    elif st.sidebar.button('Таджикистан'):
+        plot_country_graph('Таджикистан', F_ad_Prob_Mod_Sev_tjk_values, years)
+    elif st.sidebar.button('Кыргызстан'):
+        plot_country_graph('Кыргызстан', F_ad_Prob_Mod_Sev_kgz_values, years)
