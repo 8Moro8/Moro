f32da00236b4f7ca54c33077858b26f6489b480c
8Moro8 <169149172+8Moro8@users.noreply.github.com>
2024-05-08 23:22:07 +0500

    Update Untitled7.py

--- Untitled7.py
+++ Untitled7.py
@@ -56,4 +56,15 @@ elif tjk_button:
 elif kgz_button:
     plot_country_graph('Кыргызстан', F_ad_Prob_Mod_Sev_kgz_values, years)
 else:
-    chart_placeholder.text("Выберите страну в боковой панели")
+    fig, ax = plt.subplots(figsize=(10, 6))
+    ax.plot(years, F_ad_Prob_Mod_Sev_kaz_values, marker='o', linestyle='-', label='Казахстан')
+    ax.plot(years, F_ad_Prob_Mod_Sev_uzb_values, marker='o', linestyle='-', label='Узбекистан')
+    ax.plot(years, F_ad_Prob_Mod_Sev_kgz_values, marker='o', linestyle='-', label='Кыргызстан')
+    ax.plot(years, F_ad_Prob_Mod_Sev_tjk_values, marker='o', linestyle='-', label='Таджикистан')
+    ax.set_title('Центральная Азия')
+    ax.set_xticks(years)
+    ax.set_yticks(np.arange(0, 0.3, 0.05))
+    ax.legend()
+    ax.grid(True)
+    st.pyplot(fig)
+
