d31288ecb06828e85a99bb649920680d712f4f5f
8Moro8 <169149172+8Moro8@users.noreply.github.com>
2024-05-09 01:11:20 +0500

    Update Untitled7.py

--- Untitled7.py
+++ Untitled7.py
@@ -3,18 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-st.markdown(
-    """
-    <style>
-    .full-width-image {
-        width: 100%;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
-st.image("1489.jpg", caption="Мой логотип", use_column_width=True)
+st.image("1489.jpg", caption="Мой логотип", use_column_width=True, output_format='PNG', width=None, **kwargs))
 st.markdown("<h1 style='text-align: center; color: red; font-family: Times New Roman;'>Данная страница представляет тренды продовольственной безопасности центральной Азии</h1>", unsafe_allow_html=True)
 st.markdown("<h1 style='text-align: center; font-family: Times New Roman;'>Чтобы увидеть график с тенденцией нажмите на соответствующую кнопку</h1>", unsafe_allow_html=True)
 st.markdown("<h1 style='text-align: center;'>←--------------------------------</h1>", unsafe_allow_html=True)
