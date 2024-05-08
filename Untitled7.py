adba7309bd29ede2b6445369037bcabf11735ef0
8Moro8 <169149172+8Moro8@users.noreply.github.com>
2024-05-08 16:43:51 +0500

    Update Untitled7.py

--- Untitled7.py
+++ Untitled7.py
@@ -27,7 +27,6 @@ excel_urls = {
 file_name = st.selectbox('Выберите файл Excel', list(excel_urls.keys()))
 
 # Загрузка данных
-@st.cache  # Кэширование данных для повышения производительности
 def load_data(file_url):
     return pd.read_excel(file_url, engine='openpyxl')
 
@@ -41,7 +40,7 @@ def process_data(df):
 
 # Обработка данных и построение графика
 df = load_data(excel_urls[file_name])
-F_ad_Prob_Mod_Sev_values = [process_data(df[df.columns[1:]])] * 4  # Одно значение повторяется 4 раза
+F_ad_Prob_Mod_Sev_values = [process_data(df[df.columns[1:]]) for _ in range(2014, 2018)]  # Одно значение для каждого года
 
 # Построение графика
 years = range(2014, 2018)
